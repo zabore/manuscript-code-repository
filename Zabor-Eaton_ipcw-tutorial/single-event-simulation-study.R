ec19425d43bf18c81cbea5b78509bae79bc6a420
Emily C. Zabor <emily.zabor@gmail.com>
2025-12-15 11:12:36 -0500

    Update single-event-simulation-study.R

--- Zabor-Eaton_ipcw-tutorial/single-event-simulation-study.R
+++ Zabor-Eaton_ipcw-tutorial/single-event-simulation-study.R
@@ -14,14 +14,14 @@ library(furrr)
 # I'll have a separate script to fit the various models
 
 
-# Source the functions
-source(here::here("code", "fns-sim-ipcw.R"))
+# Source the functions, will need to replace below with your correct filepath
+source(here::here("code", "single-event-functions.R"))
 
 
 # Now run the simulation ----
 set.seed(20240425) 
 
-M <- 500
+M <- 5000
 n <- 500 
 alpha <- 0.05
 x_prop <- 0.5
@@ -38,13 +38,14 @@ sim_dat <-
     ~ gen_sim_data(n, alpha, x_prop, a, sigma, beta, lambda, phi)
   )
 
+# Set this up appropriately for your machine
 plan(multicore(workers = 100))
 
-# This returns a list of tibbles
+# This returns a list of tibbles with the weights added
 ipcw_dat <-
   furrr::future_map(
     sim_dat,
-    ~get_ipcw(.x)
+    ~get_ipcw_wgt(.x)
   )
 
 # Real survival probability
@@ -112,8 +113,58 @@ ipcw_boot_dat <-
         1:B, 
         function(y) 
           slice_sample(x, prop = 1, replace = T)
-      )
+      ),
+    .options = furr_options(seed = TRUE)
   ) |> 
   future_map(
     ~map(.x, ~get_ipcw_wgt(.))
+  )
+
+# Apply the two models to each of the bootstrap datasets
+# Returns a list of length M for each simulated dataset
+# Each list element is a dataframe with B rows for a Cox model fit on a bootstrap sample
+cox_boot_fit <-
+  ipcw_boot_dat |> 
+  map(
+    ~map(.x, ~get_cox_fit(.))
+  ) |> 
+  map(
+    ~list_rbind(.x, names_to = "b")
+  )
+
+ipcw_cox_boot_fit <-
+  ipcw_boot_dat |> 
+  map(
+    ~map(.x, ~get_ipcw_cox_fit(., "wgt"))
+  ) |> 
+  map(
+    ~list_rbind(.x, names_to = "b")
+  )
+
+# Calculate the variance
+# Returns a vector of length M - one bootstrap variance for each of the M simulated datasets
+cox_boot_var <- 
+  cox_boot_fit |> 
+  map_dbl(
+    ~get_boot_var(., B = B)
+  )
+
+ipcw_cox_boot_var <- 
+  ipcw_cox_boot_fit |> 
+  map_dbl(
+    ~get_boot_var(., B = B)
+  )
+
+# Calculate the 95% percentile intervals
+# Returns a dataframe with M rows and a column for the lower and upper percentile interval
+cox_boot_pci <- 
+  cox_boot_fit |> 
+  map_dfr(
+    ~get_boot_pci(.)
+  )
+
+ipcw_cox_boot_pci <- 
+  ipcw_cox_boot_fit |> 
+  map_dfr(
+    ~get_boot_pci(.)
   )
